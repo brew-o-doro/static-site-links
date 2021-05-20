649fdf21204df045502491c82385330cf7a5746b
Teodoro G <tedtke514@gmail.com>
2021-05-20 15:29:57 -0500

    set up links container file
    Created links container folder and file with an initial hardcoded link for testing

---
+++ my-static-links/src/containers/Links.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Links() {
+    return (
+        <div>
+            <h1>GitHub</h1>
+            <p>view my recent projects source code</p>
+            <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a>
+
+            
+        </div>
+    )
+}
