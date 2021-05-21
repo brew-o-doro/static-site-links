4759c6d313e2fe49ff3d9f9945fffc773ca0a4dc
Teodoro G <tedtke514@gmail.com>
2021-05-21 07:51:42 -0500

    abstracted away complexity in file
    Removed all complex files for simplicity in this static site

--- my-static-links/src/containers/Links.js
+++ my-static-links/src/containers/Links.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Links() {
+
     return (
         <div>
-            <h1>GitHub</h1>
-            <p>view my recent projects source code</p>
-            <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a>
+            <>
+                <h1>GitHub</h1>
+                <p>view my recent projects source code</p>
+                <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
+            </>
+            <>
+                <div>
+                    <h1>GitHub</h1>
+                    <p>view my recent projects source code</p>
+                    <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
+                </div>
+            </>
+            <>
+                <div>
+                    <h1>GitHub</h1>
+                    <p>view my recent projects source code</p>
+                    <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
+                </div>
+            </>
+            <>
+                <div>
+                    <h1>GitHub</h1>
+                    <p>view my recent projects source code</p>
+                    <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
+                </div>
+            </>
 
-            
         </div>
+
+        
+
+
+
     )
 }
