b2444f18df07a7689ada8825372715416b5daa5b
Teodoro G <tedtke514@gmail.com>
2021-05-21 08:39:50 -0500

    implemented semantic ui for styling

--- my-static-links/src/containers/Links.js
+++ my-static-links/src/containers/Links.js
@@ -1,42 +1,45 @@
 import React from 'react';
+import { Header, Button, Card, Image } from 'semantic-ui-react';
 
 
-export default function Links() {
+const Links = () => (
+            <><Header as='h1'>Hello, my name is Teodoro Garcia!</Header>
+            <Header as='h2'>Second Header</Header>
+            
+            <Card.Group>
+                <Card>
+                    <Card.Content>
+                        <Image floated='right'size='mini' src='https://clipground.com/images/github-logo-svg-6.jpg'/>
+                        <Card.Header>GitHub</Card.Header>
+                        <Card.Description>view my recent projects source code</Card.Description>
+                        <Card.Meta><a href='https://github.com/tedtke'>Click Me!</a></Card.Meta>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Image floated='right'size='mini' src='https://clipground.com/images/github-logo-svg-6.jpg'/>
+                        <Card.Header>GitHub</Card.Header>
+                        <Card.Description>view my recent projects source code</Card.Description>
+                        <Card.Meta><a href='https://github.com/tedtke'>Click Me!</a></Card.Meta>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Image floated='right'size='mini' src='https://clipground.com/images/github-logo-svg-6.jpg'/>
+                        <Card.Header>GitHub</Card.Header>
+                        <Card.Description>view my recent projects source code</Card.Description>
+                        <Card.Meta><a href='https://github.com/tedtke'>Click Me!</a></Card.Meta>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Image floated='right'size='mini' src='https://clipground.com/images/github-logo-svg-6.jpg'/>
+                        <Card.Header>GitHub</Card.Header>
+                        <Card.Description>view my recent projects source code</Card.Description>
+                        <Card.Meta><a href='https://github.com/tedtke'>Click Me!</a></Card.Meta>
+                    </Card.Content>
+                </Card>
+            </Card.Group></>
+)
 
-    return (
-        <div>
-            <>
-                <h1>GitHub</h1>
-                <p>view my recent projects source code</p>
-                <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
-            </>
-            <>
-                <div>
-                    <h1>GitHub</h1>
-                    <p>view my recent projects source code</p>
-                    <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
-                </div>
-            </>
-            <>
-                <div>
-                    <h1>GitHub</h1>
-                    <p>view my recent projects source code</p>
-                    <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
-                </div>
-            </>
-            <>
-                <div>
-                    <h1>GitHub</h1>
-                    <p>view my recent projects source code</p>
-                    <a href="https://github.com/tedtke" target="_blank" rel="noopener noreferrer">Click here</a> 
-                </div>
-            </>
-
-        </div>
-
-        
-
-
-
-    )
-}
+export default Links;
