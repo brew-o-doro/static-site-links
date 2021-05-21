d443e6151b2861e1eecb50e172ee5894398d8461
Teodoro G <tedtke514@gmail.com>
2021-05-21 09:16:38 -0500

    styling improvements to home page

--- my-static-links/src/containers/Links.js
+++ my-static-links/src/containers/Links.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Header, Card, Image } from 'semantic-ui-react';
 
-
 const Links = () => (
             <><Header as='h1'>Hello, my name is Teodoro Garcia.</Header>
             <Header as='h4'>I am a full-stack software engineer, content creator, and official GitKraken Ambassador!</Header>
             <Card.Group>
-                <Card>
+                <Card fluid color='red'>
                     <Card.Content href='https://github.com/tedtke'>
                         <Image floated='right'size='mini' src='https://clipground.com/images/github-logo-svg-6.jpg'/>
                         <Card.Header>GitHub</Card.Header>
@@ -14,28 +13,31 @@ const Links = () => (
                     </Card.Content>
                     
                 </Card>
-                <Card>
+                <Card fluid color='red'>
                     <Card.Content href='https://www.gitkraken.com/invite/5mvwUFjE'>
                         <Image floated='right'size='mini' src='https://tse3.mm.bing.net/th?id=OIP.VPSG-eEgwHOcjk8zO73EEQHaHa&pid=Api'/>
                         <Card.Header>GitKraken</Card.Header>
                         <Card.Description>Use my link to get the GitKraken Git GUI, GitKraken Boards &amp; GitKraken Timelines FREE! We'll both be entered to win $100 Amazon gift cards! 🐙</Card.Description>
                     </Card.Content>
                 </Card>
-                <Card>
+                <Card fluid color='red'>
                     <Card.Content href='https://medium.com/@tedtke514'>
                         <Image floated='right'size='mini' src='https://tse3.mm.bing.net/th?id=OIP.yDRgCcndS1hPqCRGsp9GsQHaHa&pid=Api'/>
                         <Card.Header>Medium Blog</Card.Header>
                         <Card.Description>Check out what I'm writing about! ✏️</Card.Description>
                     </Card.Content>
                 </Card>
-                <Card>
+                <Card fluid color='red'>
                     <Card.Content href='https://ko-fi.com/tedtke'>
                         <Image floated='right'size='mini' src='https://help.ko-fi.com/hc/article_attachments/360028221533/ko-fi_logo.png'/>
                         <Card.Header>Ko-fi</Card.Header>
                         <Card.Description>Buy me a cup of coffee ☕️</Card.Description>
                     </Card.Content>
                 </Card>
-            </Card.Group></>
+            </Card.Group>
+            <Header as='h4' >
+             made with ❤️ by tedtke
+           </Header></>
 )
 
 export default Links;
